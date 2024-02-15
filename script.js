709a9b3b65e3ddd67bca03350fa9dcbd83265857
SerjioBlack <serjioblack@gmail.com>
2024-02-15 14:13:43 +0200

    returned to simple scan

--- script.js
+++ script.js
@@ -1,66 +1,3 @@
-// var webkam = {
-//   // (A) INITIALIZE
-//   worker : null, // tesseract worker
-//   hVid : null, hGo :null, hRes : null, // html elements
-//   init : () => {
-//     // (A1) GET HTML ELEMENTS
-//     webkam.hVid = document.getElementById("vid"),
-//     webkam.hGo = document.getElementById("go"),
-//     webkam.hRes = document.getElementById("result");
-
-//     // (A2) GET USER PERMISSION TO ACCESS CAMERA
-//     navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' }})
-//     .then(async (stream) => {
-//       // (A2-1) CREATE ENGLISH TESSERACT WORKER
-//       webkam.worker = await Tesseract.createWorker();
-//       await webkam.worker.loadLanguage("eng");
-//       await webkam.worker.initialize("eng");
-
-//       // (A2-2) WEBCAM LIVE STREAM
-//       webkam.hVid.srcObject = stream;
-//       webkam.hGo.onclick = webkam.snap;
-//     })
-//     .catch(err => console.error(err));
-//   },
-
-//   // (B) SNAP VIDEO FRAME TO TEXT
-//   snap : async () => {
-//     // (B1) CREATE NEW CANVAS
-//     let canvas = document.createElement("canvas"),
-//         ctx = canvas.getContext("2d"),
-//         vWidth = webkam.hVid.videoWidth,
-//         vHeight = webkam.hVid.videoHeight;
-  
-//     // (B2) CAPTURE VIDEO FRAME TO CANVAS
-//     canvas.width = vWidth;
-//     canvas.height = vHeight;
-//     ctx.drawImage(webkam.hVid, 0, 0, vWidth, vHeight);
-
-//     // (B3) CANVAS TO IMAGE, IMAGE TO TEXT
-//     const res = await webkam.worker.recognize(canvas.toDataURL("image/png"));
-//     webkam.hRes.value = res.data.text;
-//   },
-// };
-// window.addEventListener("load", webkam.init);
-
-
-
-function filterLicenseInfo(text) {
-  // Ищем соответствие шаблону для информации о водительском удостоверении
-  const regex = /DRIVER LICENSE\s*[\s\S]*?ID:\s*(\d+)[\s\S]*?DOB:\s*(\d+-\d+-\d+)[\s\S]*?ISSUED\s*:?(\d+-\d+-\d+|ISS\s*\d+)/;
-  const match = regex.exec(text);
-
-  if (match) {
-    const id = match[1] || "ID не найден";
-    const dob = match[2] || "Дата рождения не найдена";
-    const issued = match[3] || "Дата выдачи не найдена";
-
-    return `DRIVER LICENSE\nID: ${id}\nDOB: ${dob} ISSUED: ${issued}`;
-  } else {
-    return "Информация о водительском удостоверении не найдена";
-  }
-}
-
 var webkam = {
   // (A) INITIALIZE
   worker : null, // tesseract worker
@@ -101,8 +38,9 @@ var webkam = {
 
     // (B3) CANVAS TO IMAGE, IMAGE TO TEXT
     const res = await webkam.worker.recognize(canvas.toDataURL("image/png"));
-    const filteredText = filterLicenseInfo(res.data.text); // Фильтрация текста
-    webkam.hRes.value = filteredText;
+    webkam.hRes.value = res.data.text;
   },
 };
 window.addEventListener("load", webkam.init);
+
+
